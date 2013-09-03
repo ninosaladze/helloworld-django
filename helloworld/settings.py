2f0e9de45490ea3aa337f63883f4d63629570b41
Matthew Fisher <matthewf@activestate.com>
2013-09-03 09:45:35 -0700

    moved wsgi app back to helloworld

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -105,7 +105,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'helloworld.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
